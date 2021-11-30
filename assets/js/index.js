3283da370872fe74cabb4997a45c6634259bef73
Xiaolin Jin Lin <x140l1n@hotmail.com>
2021-12-01 00:18:07 +0100

    Create ranking.

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,18 +12,7 @@ function init() {
     board.style.width = `${BOARD_WIDTH}px`;
     board.style.height = `${BOARD_HEIGHT}px`;
 
-    //Trigger transitions.
-    let menu_options = document.querySelector("#menu-options");
-    [].slice.call(menu_options.children).forEach((element) => element.style.left = 0);
-
-    let logo = document.querySelector("#logo");
-    logo = [].slice.call(logo.children);
-    logo[0].style.left = 0;
-    logo[1].style.top = 0;
-    logo[2].style.right = 0;
-
-    let menu_footer = document.querySelector("#menu-footer");
-    menu_footer.firstElementChild.style.left = 0;
+    start_transitions();
 
     let main_menu = document.querySelector("#main-menu");
 
@@ -51,6 +40,8 @@ function init() {
                 let ranking_menu = document.querySelector("#ranking-menu");
                 ranking_menu.style.display = "flex";
 
+                get_ranking();
+
                 break;
 
             case "go-back":
@@ -66,6 +57,124 @@ function init() {
                 break;
         }
     });
-    
+
     //initGame();
+}
+
+function start_transitions() {
+    let menu_options = document.querySelector("#menu-options");
+    [].slice
+        .call(menu_options.children)
+        .forEach((element) => (element.style.left = 0));
+
+    let logo = document.querySelector("#logo");
+    logo = [].slice.call(logo.children);
+    logo[0].style.left = 0;
+    logo[1].style.top = 0;
+    logo[2].style.right = 0;
+
+    let menu_footer = document.querySelector("#menu-footer");
+    menu_footer.firstElementChild.style.left = 0;
+}
+
+function get_ranking() {
+    let data = new FormData();
+    data.append("action", "get");
+    data.append("id_game", 1);
+    data.append("top", 10);
+    data.append("id_user", 1);
+
+    let table_ranking = document.querySelector("#table-ranking");
+    table_ranking.style.height = "100%";
+    let body = table_ranking.tBodies[0];
+    let footer = table_ranking.tFoot;
+    body.innerHTML = "";
+    footer.innerHTML = "";
+
+    let row_wait = document.createElement("tr");
+    let cell_wait = document.createElement("td");
+    cell_wait.innerText = "Espere...";
+    row_wait.appendChild(cell_wait);
+    body.appendChild(row_wait);
+
+    fetch("./backend/ranking.php", {
+        method: "POST",
+        cache: "no-cache",
+        body: data,
+    })
+    .then(response => response.json())
+    .then(data => {
+        table_ranking.style.height = "";
+
+        create_ranking(body, footer, data, 1);
+    })
+    .catch(error => console.error(error));
+}
+
+function create_ranking(body, footer, data, id_user) {
+    body.innerHTML = "";
+
+    if (data.data.length > 0) {
+        let isTop = false; //If the user is in the top 10.
+
+        data.data.forEach((item, index) => {
+            let position = index + 1;
+            let nickname = Number.parseInt(item.id) === id_user ? "Tu": item.nickname;
+            let time = item.time;
+
+            let row = document.createElement("tr");
+
+            if (nickname === "Tu") {
+                row.style.backgroundColor = "#1b61a7";
+                isTop = true;
+            }
+
+            let cell_position = document.createElement("td");
+
+            if (position >= 1 && position <= 3) {
+                let position_medal = document.createElement("img");
+                position_medal.src = `./assets/img/${position}.png`;
+                position_medal.width = 30;
+
+                cell_position.appendChild(position_medal);
+            } else {
+                cell_position.innerText = position;
+            }
+
+            let cell_nickname = document.createElement("td");
+            cell_nickname.innerText = nickname;
+
+            let cell_time = document.createElement("td");
+
+            cell_time.innerText = time;
+
+            row.appendChild(cell_position);
+            row.appendChild(cell_nickname);
+            row.appendChild(cell_time);
+
+            body.appendChild(row);
+        });
+
+        //Add user ranking in footer of table if there is not in the top 10.
+        if (!isTop && data.data_user !== null) {
+            let row = document.createElement("tr");
+            row.style.backgroundColor = "#1b61a7";
+
+            let cell_position = document.createElement("td");
+            cell_position.innerText = "-";
+            
+            let cell_nickname = document.createElement("td");
+            cell_nickname.innerText = "Tu";
+
+            let cell_time = document.createElement("td");
+
+            cell_time.innerText = data.data_user.time;
+
+            row.appendChild(cell_position);
+            row.appendChild(cell_nickname);
+            row.appendChild(cell_time);
+            
+            footer.appendChild(row);
+        }
+    }
 }
---
+++ backend/ranking.php
@@ -0,0 +1,88 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+//Import the librarys.
+require_once("response.php");
+require_once("constants.php");
+require_once("database.php");
+
+//If the action is defined and is not empty.
+if (isset($_POST["action"]) && !empty($_POST["action"])) {
+    //Get the action name.
+    $action = $_POST["action"];
+
+    //Check if the action exists.
+    if (function_exists($action)) {
+        //Call the action.
+        $action();
+    } else {
+        Response::send(array("msg" => "Function not exists."), Response::HTTP_METHOD_NOT_ALLOWED);
+    }
+} else {
+    Response::send(array("msg" => "Param 'action' missing."), Response::HTTP_METHOD_NOT_ALLOWED);
+}
+
+
+function get()
+{
+    $ranking = [];
+    $ranking_user = null;
+
+    $top = isset($_POST["top"]) ? $_POST["top"] : 10;
+    $id_game = isset($_POST["id_game"]) ? $_POST["id_game"] : -1;
+    $id_user = isset($_POST["id_user"]) ? $_POST["id_user"] : -1;
+
+    try {
+        if ($id_game !== -1 && $id_user !== -1) {
+            $db = new Database();
+
+            $statement = $db->connect()->prepare("SELECT u.id, u.nickname, DATE_FORMAT(ugc.time, \"%H:%i\") as time
+                                                  FROM user_game_cycle ugc
+                                                  INNER JOIN user u ON u.id = ugc.id_user
+                                                  WHERE ugc.id_game = :id_game
+                                                  ORDER BY ugc.time ASC
+                                                  LIMIT :top");
+
+            $statement->bindParam(":id_game", $id_game);
+            $statement->bindParam(":top", $top);
+
+            $statement->execute();
+
+            $inTop = false;
+            
+            foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $row) {
+
+                if ($row["id"] == $id_user) {
+                    $inTop = true;
+                }
+
+                array_push($ranking, array("id" => $row["id"], "nickname" => $row["nickname"], "time" => $row["time"]));
+            }
+
+            //If the user is not in the top 10.
+            if (!$inTop) {
+                $statement = $db->connect()->prepare("SELECT u.id, u.nickname, DATE_FORMAT(ugc.time, \"%H:%i\") as time
+                                                      FROM user_game_cycle ugc
+                                                      INNER JOIN user u ON u.id = ugc.id_user
+                                                      WHERE ugc.id_game = :id_game AND ugc.id_user = :id_user
+                                                      LIMIT 1");
+
+                $statement->bindParam(":id_game", $id_game);
+                $statement->bindParam(":id_user", $id_user);
+
+                $statement->execute();
+
+                if ($statement->rowCount() > 0) {
+                    $ranking_user = $statement->fetch(PDO::FETCH_ASSOC);
+                }
+            }
+
+            Response::send(array("msg" => "Ok.", "data" => $ranking, "data_user" => $ranking_user), Response::HTTP_OK);
+        } else {
+            Response::send(array("msg" => "Game not found."), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+    } catch (PDOException $e) {
+        Response::send(array("msg" => "An unexpected error has occurred: " . $e->getMessage()), Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
